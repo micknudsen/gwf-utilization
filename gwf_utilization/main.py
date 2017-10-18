f348e444ec546888e1687d9954245e9bb39ffc8d
Michael Knudsen <michaelk@clin.au.dk>
2017-10-18 19:08:39 +0200

    Get job_ids of all targets

--- gwf_utilization/main.py
+++ gwf_utilization/main.py
@@ -1,9 +1,9 @@
 import click
 
 from gwf.core import graph_from_config
-from gwf.backends import backend_from_config
-from gwf.backends.slurm import SlurmBackend
 from gwf.exceptions import GWFError
+from gwf.backends import backend_from_config
+from gwf.backends.slurm import SlurmBackend, _call_generic
 
 
 @click.command()
@@ -13,3 +13,9 @@ def utilization(obj):
     backend_cls = backend_from_config(obj)
     if not issubclass(backend_cls, SlurmBackend):
         raise GWFError('Utilization plugin only works for Slurm backend!')
+
+    graph = graph_from_config(obj)
+
+    with backend_cls() as backend:
+        for target in graph.targets.values():
+            job_id = backend.get_job_id(target)
