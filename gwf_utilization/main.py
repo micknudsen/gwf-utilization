1910a1fcd2a55ab697a2739a072e435e5c0dd3e7
Michael Knudsen <michaelk@clin.au.dk>
2017-10-18 13:56:09 +0200

    Configured setuptools

---
+++ gwf_utilization/main.py
@@ -0,0 +1,6 @@
+import click
+
+
+@click.command()
+def utilization():
+    print('Hello, World!')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='gwf-utilization',
+
+    packages=find_packages(),
+
+    entry_points = {'gwf.plugins': ['utilization = gwf_utilization.main:utilization']}
+
+)
