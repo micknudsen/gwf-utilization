4c7a1ff08ac8950fcd14675af6f551a5c8a6d8f6
Michael Knudsen <michaelk@clin.au.dk>
2017-10-18 15:12:06 +0200

    Raise exception if backend is not Slurm

--- gwf_utilization/main.py
+++ gwf_utilization/main.py
@@ -1,6 +1,14 @@
 import click
 
+from gwf.core import graph_from_config
+from gwf.backends import backend_from_config
+from gwf.backends.slurm import SlurmBackend
+from gwf.exceptions import GWFError
+
 
 @click.command()
-def utilization():
-    print('Hello, World!')
+@click.pass_obj
+def utilization(obj):
+    # This plugin only works for SlurmBackend
+    if not issubclass(backend_from_config(obj), SlurmBackend):
+        raise GWFError('Utilization plugin only works for Slurm backend!')
