4e59f1c847ad35744388bcddd5b7e6451a2d52d2
Michael Knudsen <michaelk@clin.au.dk>
2017-10-18 20:45:10 +0200

    Allow user to specify targets

--- gwf_utilization/main.py
+++ gwf_utilization/main.py
@@ -2,21 +2,26 @@ import click
 
 from gwf.core import graph_from_config
 from gwf.exceptions import GWFError
+from gwf.filtering import filter_names
 from gwf.backends import backend_from_config
 from gwf.backends.slurm import SlurmBackend
 
 from gwf_utilization.accounting import call_sacct
 
 @click.command()
+@click.argument('targets', nargs=-1)
 @click.pass_obj
-def utilization(obj):
+def utilization(obj, targets):
     # This plugin only works for SlurmBackend
     backend_cls = backend_from_config(obj)
     if not issubclass(backend_cls, SlurmBackend):
         raise GWFError('Utilization plugin only works for Slurm backend!')
 
     graph = graph_from_config(obj)
+    matches = graph.targets.values()
+    if targets:
+        matches = filter_names(matches, targets)
 
     with backend_cls() as backend:
-        job_ids = [backend.get_job_id(target) for target in graph.targets.values()]
+        job_ids = [backend.get_job_id(target) for target in matches]
         print(call_sacct(job_ids))
