99930f65cf15a95d8bcaf3f9349dfce691357350
Michael Knudsen <michaelk@clin.au.dk>
2017-10-20 10:21:18 +0200

    Parse sacct output

--- gwf_utilization/accounting.py
+++ gwf_utilization/accounting.py
@@ -1,10 +1,13 @@
+import pandas as pd
+
 from gwf.backends.slurm import _call_generic
 
-
-def call_sacct(job_ids):
-   return _call_generic('sacct', '--noheader', '--long', '--parsable2', '--allocations', '--jobs', ','.join(job_ids))
 
 class Accountant:
 
     def __init__(self, job_ids):
-        self.job_ids = job_ids
+
+        sacct_output =  _call_generic('sacct', '--long', '--parsable2', '--allocations', '--jobs', ','.join(job_ids))
+        columns, *data = [line.split('|') for line in sacct_output.splitlines()]
+
+        self._df = pd.DataFrame(data=data, columns=columns)
