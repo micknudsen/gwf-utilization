20b86042e52551118fab63176f141f5b8a740a95
Michael Knudsen <michaelk@clin.au.dk>
2017-10-27 13:22:41 +0200

    Added function for converting time strings to seconds

--- gwf_utilization/accounting.py
+++ gwf_utilization/accounting.py
@@ -1,4 +1,23 @@
+import re
+
 from gwf.backends.slurm import _call_generic
+
+
+def seconds(time_string):
+    '''Converts time string on the form DD-HH:MM:SS to seconds'''
+
+    r = r'(?:([0-9]+)-)?([0-9]{2}):([0-9]{2}):([0-9]{2})'
+
+    days, hours, minutes, seconds = re.match(r, time_string).groups()
+
+    result = int(seconds)
+    result += 60 * int(minutes)
+    result += 3600 * int(hours)
+
+    if days:
+        result += 86400 * int(days)
+
+    return result
 
 
 class Accountant:
