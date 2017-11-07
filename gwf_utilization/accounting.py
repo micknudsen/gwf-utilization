1b86feb8f850507547041f491a4ec643187e0932
Michael Knudsen <michaelk@clin.au.dk>
2017-11-07 20:36:11 +0100

    Made seconds a static method in Accountant class

--- gwf_utilization/accounting.py
+++ gwf_utilization/accounting.py
@@ -1,23 +1,6 @@
 import re
 
 
-def seconds(time_string):
-    '''Converts time string on the form DD-HH:MM:SS to seconds'''
-
-    r = r'(?:([0-9]+)-)?([0-9]{2}):([0-9]{2}):([0-9]{2})'
-
-    days, hours, minutes, seconds = re.match(r, time_string).groups()
-
-    result = int(seconds)
-    result += 60 * int(minutes)
-    result += 3600 * int(hours)
-
-    if days:
-        result += 86400 * int(days)
-
-    return result
-
-
 class Accountant:
 
     def __init__(self, jobs):
@@ -27,3 +10,19 @@ class Accountant:
     def from_sacct_output(cls, sacct_output):
         sacct_columns ,*sacct_data = [line.split('|') for line in sacct_output.splitlines()]
         return cls(jobs=[dict(zip(sacct_columns, entry)) for entry in sacct_data])
+
+    @staticmethod
+    def seconds(time_string):
+        '''Converts time string on the form DD-HH:MM:SS to seconds'''
+
+        time_regexp = r'(?:([0-9]+)-)?([0-9]{2}):([0-9]{2}):([0-9]{2})'
+        days, hours, minutes, seconds = re.match(time_regexp, time_string).groups()
+
+        result = int(seconds)
+        result += 60 * int(minutes)
+        result += 3600 * int(hours)
+
+        if days:
+            result += 86400 * int(days)
+
+        return result
