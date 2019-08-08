05103e1363e53283369af08dfc98be6546d5df45
Michael Knudsen <michaelk@clin.au.dk>
2019-08-08 11:37:28 +0200

    Implemented _call_sacct function

--- src/gwf_utilization/accounting.py
+++ src/gwf_utilization/accounting.py
@@ -55,6 +55,14 @@ def _parse_memory_string(memory_string, cores, nodes):
     return raw_result
 
 
+def _call_sacct(job_id):
+    return _call_generic(
+        'sacct',
+        '--format=' + ','.join(SLURM_SACCT_COLS),
+        '--parsable2',
+        f'--jobs {job_id}'
+    )
+
 def _call_sacct_batch(job_ids):
     return _call_generic(
         'sacct',
